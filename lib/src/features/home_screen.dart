4955c5815ffc3ad48f90348331300e9bbf3dad5a
Manish sahu <122163664+Manishs2261@users.noreply.github.com>
2023-12-27 16:14:38 +0530

    feat: custome tab bar

--- lib/src/features/home_screen.dart
+++ lib/src/features/home_screen.dart
@@ -10,8 +10,60 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                ),
+                child: TabBar(
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      25.0,
+                    ),
+                    color: Colors.green,
+                  ),
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.black,
+                  tabs: [
+                    Tab(
+                      text: 'Place Bid',
+
+                    ),
+                    Tab(
+                      text: 'Buy Now',
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                  child: TabBarView(
+                children: [
+                  Center(
+                    child: Text("set notification"),
+                  ),
+                  Center(
+                    child: Text("watch notification"),
+                  ),
+                ],
+              ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
