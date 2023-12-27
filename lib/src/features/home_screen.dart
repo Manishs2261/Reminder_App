05f52088c724a6ce0be8a1c7ea249a1a4ffac423
Manish sahu <122163664+Manishs2261@users.noreply.github.com>
2023-12-27 22:27:57 +0530

    feat: add reminder list

--- lib/src/features/home_screen.dart
+++ lib/src/features/home_screen.dart
@@ -8,49 +8,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-
-  String? _dropDownValue;
-
+  TimeOfDay initialTime = TimeOfDay.now();
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.white12,
         appBar: AppBar(
-          backgroundColor: Colors.black,
-          titleTextStyle: TextStyle(color: Colors.white,fontSize: 30),
-          title: Text("Home"),
+          backgroundColor: Colors.black12,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 30),
+          title: Text("Reminder"),
         ),
         body: Padding(
-          padding: const EdgeInsets.only(top: 8.0,bottom: 8.0,left: 15.0,right: 15.0),
+          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 15.0, right: 15.0),
           child: Column(
             children: [
               Container(
                 height: 45,
                 decoration: BoxDecoration(
-                  color: Colors.grey[300],
+                  color: Colors.grey.shade800,
                   borderRadius: BorderRadius.circular(
                     25.0,
                   ),
                 ),
                 child: TabBar(
-
                   indicatorSize: TabBarIndicatorSize.tab,
                   indicator: BoxDecoration(
                     borderRadius: BorderRadius.circular(
                       25.0,
                     ),
-                    color: Colors.green,
+                    color: Colors.white70,
                   ),
-                  labelColor: Colors.white,
-                  unselectedLabelColor: Colors.black,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.white,
                   tabs: [
                     Tab(
                       text: 'Watch reminder',
-
                     ),
                     Tab(
                       text: 'set reminder',
@@ -61,22 +56,56 @@ class _HomeScreenState extends State<HomeScreen> {
               Expanded(
                   child: TabBarView(
                 children: [
-
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.only(left: 8, right: 8, top: 20),
                     child: Column(
                       children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          height: 80,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              boxShadow: [
+                                BoxShadow(color: Colors.grey.shade600, spreadRadius: 0.8),
+                              ],
+                              color: Colors.grey.shade800),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text("07.55 AM",style: TextStyle(color: Colors.white,fontSize: 24),),
+                                  SizedBox(width: 15,),
+                                  Text("Monday",style: TextStyle(color: Colors.white70,fontSize: 16),),
 
+                                ],
+                              ),
+
+                              Text("Go to Office",style: TextStyle(color: Colors.white60,fontSize: 20),),
+
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8, top: 40),
+                    child: Column(
+                      children: [
                         Container(
                           padding: EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                              color: Colors.white, borderRadius: BorderRadius.circular(10)),
+                          decoration:
+                              BoxDecoration(color: Colors.grey.shade800, borderRadius: BorderRadius.circular(10)),
                           child: DropdownButtonFormField<String>(
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                            dropdownColor: Colors.grey.shade800,
                             decoration: InputDecoration(
-                                border: InputBorder.none
+                              border: InputBorder.none,
                             ),
                             borderRadius: BorderRadius.circular(10),
-
                             value: 'Monday',
                             items: [
                               'Monday',
@@ -95,24 +124,58 @@ class _HomeScreenState extends State<HomeScreen> {
                             onChanged: (day) {
                               // Handle day selection
                             },
-
                           ),
                         ),
-                        SizedBox(height: 10,),
-
-
-                        SizedBox(height: 10,),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 15, right: 15),
+                          height: 48,
+                          decoration:
+                              BoxDecoration(color: Colors.grey.shade800, borderRadius: BorderRadius.circular(10)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "${initialTime.hour}:${initialTime.minute}",
+                                style: TextStyle(color: Colors.white, fontSize: 18),
+                              ),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.grey.shade700,
+                                  ),
+                                  onPressed: () async {
+                                    final timeOfDay = await showTimePicker(
+                                      context: context,
+                                      initialTime: initialTime,
+                                    );
+                                    if (timeOfDay != null) {
+                                      setState(() {
+                                        initialTime = timeOfDay;
+                                      });
+                                    }
+                                  },
+                                  child: Text(
+                                    "Choose Time",
+                                  ))
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
                         Container(
                           padding: EdgeInsets.symmetric(horizontal: 10),
                           decoration: BoxDecoration(
-                              color: Colors.white, borderRadius: BorderRadius.circular(10)),
-                          child: DropdownButtonFormField<String>(
-
-                            decoration: InputDecoration(
-                                border: InputBorder.none
-                            ),
+                            color: Colors.grey.shade800,
                             borderRadius: BorderRadius.circular(10),
-
+                          ),
+                          child: DropdownButtonFormField<String>(
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                            dropdownColor: Colors.grey.shade800,
+                            decoration: InputDecoration(border: InputBorder.none),
+                            borderRadius: BorderRadius.circular(10),
                             value: 'Wake up',
                             items: [
                               'Wake up',
@@ -133,16 +196,25 @@ class _HomeScreenState extends State<HomeScreen> {
                             onChanged: (day) {
                               // Handle day selection
                             },
-
                           ),
                         ),
-
+                        SizedBox(
+                          height: 70,
+                        ),
+                        SizedBox(
+                            height: 40,
+                            width: double.infinity,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.grey.shade700,
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  "Set Reminder",
+                                  style: TextStyle(fontSize: 16),
+                                )))
                       ],
                     ),
-                  ),
-
-                  Center(
-                    child: Text("watch notification"),
                   ),
                 ],
               ))
